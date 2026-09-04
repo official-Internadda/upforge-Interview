1e7b493c86dbac2c14d82157f4c1fe43ce4fa14a
official-Internadda <official.internadda@gmail.com>
2026-09-04 10:23:28 +0530

    Update firebase.js

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -15,3 +15,8 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
+
+fetch("https://upforge-ai-interviewer-backend.onrender.com")
+  .catch(() => {});
